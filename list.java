ca54cf292a2254e530905150510c1374cc39f581
Dharanika19 <ddharanika74@gmail.com>
2024-06-24 16:55:54 +0530

    addlast

---
+++ list.java
@@ -0,0 +1,18 @@
+import java.util.LinkedList;
+public class list{
+    public static void main(String args[]){
+        LinkedList<String> program=new LinkedList<>();
+        program.add("c++");
+        program.add("java");
+        program.add("html");
+        program.add("PHP");
+        program.addLast("SOL");
+
+        System.out.println(program);
+    }
+}
+
+
+
+    
+
