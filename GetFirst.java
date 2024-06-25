59304b81fa26292b806e760ce1654dae441afdc2
Dharanika19 <ddharanika74@gmail.com>
2024-06-25 14:24:30 +0530

    GetFirst.java

---
+++ GetFirst.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+public class GetFirst {
+    public static void main(String args[]){
+        LinkedList<String> fruits=new LinkedList<>();
+        fruits.add("apple");
+        fruits.add("orange");
+        fruits.add("mango");
+        fruits.add("banana");
+        System.out.println(fruits.getFirst());                                                              
+      }
+    }
+
