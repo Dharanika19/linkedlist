1d685428178f6347933a2e6e864ef0858e0808ca
Dharanika19 <ddharanika74@gmail.com>
2024-06-25 14:29:14 +0530

    Size.java

---
+++ Size.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class Size{
+    public static void main(String args[]){
+        LinkedList<String>laptop=new LinkedList<>();
+        laptop.add("lenova");
+        laptop.add("Hp");
+        laptop.add("dell");
+        laptop.add("acer");
+
+        System.out.println(laptop.size());
+    }
+}
+    
+
