26f1626b5cf10c85e793e05717fed2899e95e59c
Dharanika19 <ddharanika74@gmail.com>
2024-06-26 22:33:13 +0530

    Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        LinkedList<String> name=new LinkedList<>();
+         name.add("Dharan");
+         name.add("kavi");
+         name.add("saran");
+         name.add("anish");
+         System.out.println(name.reversed());
+    }
+}
